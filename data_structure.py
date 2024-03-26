6293df6459c7e775cf924da7695cc00c6bfa4c70
sunday amos daniel <139694702+amosdan10@users.noreply.github.com>
2024-03-27 00:05:21 +0100

    Add files via upload

---
+++ data_structure.py
@@ -0,0 +1,9 @@
+my_list = []
+my_list.append(10,20,30,40)
+my_list.insert(1, 15)
+#print(my_list)
+my_list.extend(50,60,70)
+my_list.remove(70)
+my_list.sort()
+index = my_list.index(2)
+print(index(my_list))
